0b0b550ddad922ec0989a4df92af43262c502828
Charl <info@charlroux.co.za>
2020-10-26 20:39:53 +0200

    Added sections to about

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Header from './Header/Header';
 import Banner from './Banner/Banner';
 import ShortenForm from './ShortenForm/ShortenForm';
+import About from './About/About';
 function App() {
   return (
     <div className="App">
         <Header/>
         <Banner/>
         <ShortenForm/>
+        <About/>
     </div>
   );
 }
