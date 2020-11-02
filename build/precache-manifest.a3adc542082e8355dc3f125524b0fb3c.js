5eb74a878c83bc430f02eac4e921b0fdc2ee83b1
Charl <info@charlroux.co.za>
2020-11-02 19:33:14 +0200

    Gitingor file edit

---
+++ build/precache-manifest.a3adc542082e8355dc3f125524b0fb3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae8e8ae98e4d6e94ba34804e6e9e753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8220c23af8f267594eb9",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9ee42ca4f567c49715ef",
+    "url": "/static/css/main.b9da2391.chunk.css"
+  },
+  {
+    "revision": "8220c23af8f267594eb9",
+    "url": "/static/js/2.e5287737.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/static/js/2.e5287737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee42ca4f567c49715ef",
+    "url": "/static/js/main.7ab1262c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
